8e745c1d4e62e10daf90b3c3020ecbf913a564bd
= <=>
2025-08-17 19:01:12 -0600

    pc

--- admin/actions/productos/todos_los_productos.php
+++ admin/actions/productos/todos_los_productos.php
@@ -48,6 +48,6 @@
     </table>
 </div>
 
-<div id="loading-indicator" style="display: none; text-align: center; ">
+<div id="loading-indicator" style="display: none; text-align: center; font-size: .8rem;">
     <p>Cargando más productos...</p>
 </div>
